1b3b18ff99ffe64cba3ceef53e2f185a970030b5
Sundar Anbu <sundar@ken42.com>
2024-05-13 00:20:47 +0530

    data

---
+++ src/app/(auth)/api/userClient/route.ts
@@ -0,0 +1,36 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { NextApiResponse, NextApiRequest } from 'next';
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const createClient = await db.userClient.create({
+      data: { ...body },
+    });
+    return NextResponse.json(
+      { client: createClient, message: "Client created successfully" },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: NextApiRequest) {
+    try {
+      const userClient = await db.userClient.findMany({
+       
+      })
+
+        return NextResponse.json({ userClient }, { status: 200 });
+    } catch (err) {
+      console.log(err);
+    
+    }
+  }
+  
--- src/app/(dashboard)/user/page.tsx
+++ src/app/(dashboard)/user/page.tsx
@@ -12,28 +12,25 @@ import { useSession } from 'next-auth/react';
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 import { useRouter } from 'next/navigation';
 
-const Page = () => {
+const UserPage = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
   const [clients, setClients] = React.useState([]);
   const [open, setOpen] = React.useState(false);
   const [formData, setFormData] = React.useState({
-    clientName: '',
-    clientEmail: '',
-    clientId: '',
-    spocName: '',
-    spocPhone: '',
-    spocEmail: '',
-    validTill: '2001-12-24T00:00:00Z',
+    //clientId:null ,
+    userName: '',
+    onBoarded: "2001-12-24T00:00:00.000Z",
+    "Active": true
   });
 
   React.useEffect(() => {
     const fetchData = async () => {
       if (status === "authenticated") {
         try {
-          const response = await fetch("/api/client");
+          const response = await fetch("/api/userClient");
           const data = await response.json();
-          setClients(data.clients);
+          setClients(data.userClient);
         } catch (error) {
           console.error("Failed to fetch client data:", error);
         }
@@ -43,24 +40,23 @@ const Page = () => {
     fetchData();
   }, [status]);
 
-  const handleInputChange = (event:any) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
+   
     setFormData({
       ...formData,
       [name]: value,
+      client: { connect: { clientId:"Flame123" } } // Updated to use parsedValue
     });
   };
+  
 
 
-  const redirectToUserTab=(clientId:any)=>{
-    console.log('clientId:', clientId);
-    router.push(`${window.location.origin}/user?clientId=${clientId}`)
-  }
 
   const handleSubmit = async () => {
     console.log('formData:', formData); 
     try {
-      const response = await fetch("/api/client", {
+      const response = await fetch("/api/userClient", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -100,15 +96,11 @@ const Page = () => {
           <TableHead sx={{ backgroundColor: '#f0f0f0' }}>
             <TableRow>
               <TableCell>S.No</TableCell>
-              <TableCell>Client Name</TableCell>
-              <TableCell align="right">Client Email</TableCell>
+              {/* <TableCell>Client Id</TableCell> */}
+              <TableCell align="right">User Name</TableCell>
               <TableCell align="right">Client ID</TableCell>
-              <TableCell align="right">SPOC Name</TableCell>
-              <TableCell align="right">SPOC Phone</TableCell>
-              <TableCell align="right">SPOC Email</TableCell>
-              <TableCell align="right">Valid Till</TableCell>
-              <TableCell align="right">Created At</TableCell>
-              <TableCell align="right">Updated At</TableCell>
+              <TableCell align="right">OnBoarded</TableCell>
+              <TableCell align="right">Active</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -118,25 +110,10 @@ const Page = () => {
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell>{index + 1}</TableCell>
-                <TableCell>
-                  <a
-                    href="#"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      redirectToUserTab(client.id);
-                    }}
-                  >
-                    {client.clientName}
-                  </a>
-                </TableCell>
-                <TableCell align="right">{client.clientEmail}</TableCell>
                 <TableCell align="right">{client.clientId}</TableCell>
-                <TableCell align="right">{client.spocName}</TableCell>
-                <TableCell align="right">{client.spocPhone}</TableCell>
-                <TableCell align="right">{client.spocEmail}</TableCell>
-                <TableCell align="right">{client.validTill}</TableCell>
-                <TableCell align="right">{client.createdAt}</TableCell>
-                <TableCell align="right">{client.updatedAt}</TableCell>
+                <TableCell align="right">{client.userName}</TableCell>
+                <TableCell align="right">{client.onBoarded}</TableCell>
+                <TableCell align="right">{client.Active}</TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -145,68 +122,48 @@ const Page = () => {
       <Dialog open={open} onClose={() => setOpen(false)}>
         <DialogTitle>Create Client</DialogTitle>
         <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="clientName"
-            name="clientName"
-            label="Client Name"
-            type="text"
-            fullWidth
-            value={formData.clientName}
-            onChange={handleInputChange}
-          />
-            <TextField
-                margin="dense"
-                id="clientEmail"
-                name="clientEmail"
-                label="Client Email"
-                type="email"
-                fullWidth
-                value={formData.clientEmail}
-                onChange={handleInputChange}
-            />
-            <TextField
-                margin="dense"
-                id="clientId"
-                name="clientId"
-                label="Client ID"
-                type="text"
-                fullWidth
-                value={formData.clientId}
-                onChange={handleInputChange}
-            />
-            <TextField
-                margin="dense"
-                id="spocName"
-                name="spocName"
-                label="SPOC Name"
-                type="text"
-                fullWidth
-                value={formData.spocName}
-                onChange={handleInputChange}
-            />
-            <TextField
-                margin="dense"
-                id="spocPhone"
-                name="spocPhone"
-                label="SPOC Phone"
-                type="text"
-                fullWidth
-                value={formData.spocPhone}
-                onChange={handleInputChange}
-            />
-            <TextField
-                margin="dense"
-                id="spocEmail"
-                name="spocEmail"
-                label="SPOC Email"
-                type="email"
-                fullWidth
-                value={formData.spocEmail}
-                onChange={handleInputChange}
-            />
+        {/* <TextField
+  margin="dense"
+  id="clientId"
+  name="clientId"
+  label="Client ID"
+  type="number"
+  fullWidth
+  value={formData.clientId === null ? '' : formData.clientId} // handle null value
+  onChange={handleInputChange}
+/> */}
            
+            <TextField
+                margin="dense"
+                id="userName"
+                name="userName"
+                label="User Name"
+                type="text"
+                fullWidth
+                value={formData.userName}
+                onChange={handleInputChange}
+            />
+            {/* <TextField
+                margin="dense"
+                id="onBoarded"
+                name="onBoarded"
+                label="OnBoarded"
+                type="date"
+                fullWidth
+                value={formData.onBoarded}
+                onChange={handleInputChange}
+            /> */}
+            <TextField
+                margin="dense"
+                id="Active"
+                name="Active"
+                label="Active"
+                type="text"
+                fullWidth
+                value={formData.Active}
+                onChange={handleInputChange}
+            />
+
 
         </DialogContent>
         <DialogActions>
@@ -218,4 +175,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default UserPage;
